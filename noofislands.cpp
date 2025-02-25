2d60ca5b68e9282be7702a4648660c84baf55910
Tanshi Mehra <tanshimehra547@gmail.com>
2025-02-26 00:32:12 +0530

    correction

--- noofislands.cpp
+++ noofislands.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<int>>&adj)
+void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&adj)
 {
     vis[row][col]=1;
     queue<pair<int,int>>q;
@@ -11,10 +11,23 @@ void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<int>>&adj)
     {
         int row=q.front().first;
         int col=q.front().second;
-        
+        q.pop();
+        for(int i=-1;i<=1;i++)
+        {
+            for(int j=-1;j<=1;j++)
+            {
+                int nrow=row+i;
+                int ncol=col+j;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol]&&adj[nrow][ncol]=='1')
+                {
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
     }
 }
-int nofisland(vector<vector<int>>&adj)
+int nofisland(vector<vector<char>>&adj)
 {
     int count=0;
     int n=adj.size();
@@ -22,10 +35,10 @@ int nofisland(vector<vector<int>>&adj)
     vector<vector<int>>vis(n,vector<int>(m,0));
     for(int row=0;row<n;row++)
     {
-        for(int col=0;co<m;col++)
+        for(int col=0;col<m;col++)
         {
             
-            if(!vis[row][col]&&adj[row][col]='1')
+            if(!vis[row][col]&&adj[row][col]=='1')
             {
                  count++;
                 bfs(row,col,vis,adj);
@@ -38,15 +51,16 @@ int main()
 {
     int n,m;
     cin>>n>>m;
-    vector<vector<char>>adj(n,vector<char>(m,'0'));
-    for(int i=0;i<m;i++)
+    vector<vector<char>>adj(n,vector<char>(m));
+    for(int i=0;i<n;i++)
     {
-        char u,v;
-        cin>>u>>v;
-        adj[u][v]='1';
-        adj[v][u]='1';
-       int iland= nofisland(adj);
+        for(int j=0;j<m;j++)
+        {
+            cin>>adj[i][j];
 
+        }
     }
+   int iland= nofisland(adj);
+
     cout<<"the number of islands are"<<iland;
 }
